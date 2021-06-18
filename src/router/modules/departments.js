123c0f6ca900ab9e1f12266c156a5d4f82d4c9c6
Jian <45423711+Heyzj@users.noreply.github.com>
2021-06-18 13:01:04 +0800

    Update departments.js
    增加了departments 子路由的name

--- src/router/modules/departments.js
+++ src/router/modules/departments.js
@@ -7,6 +7,7 @@ export default {
   children: [
     {
       path: '',
+      name: 'departments',
       component: () => import('@/views/departments/index'),
       meta: {
         title: '组织架构',
